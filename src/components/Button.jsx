dc3d39efddc506cb95a76578290148f8c789c301
Ilmaooo <ilmaogresevic1@gmail.com>
2023-09-10 14:39:10 +0200

    button modified

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -6,7 +6,7 @@ function Button() {
   return (
     <button
       onClick={generatePDF}
-      className="absolute top-12 right-10 rounded-xl py-1 px-3 font-[montserrat] font-medium border-solid border-[#979191] bg-[#979191] text-white tracking-wide md:mr-80"
+      className="absolute top-20 right-10 rounded-xl py-1 px-3 font-[montserrat] font-medium border-solid border-[#979191] bg-[#979191] text-white tracking-wide md:mr-80"
     >
       Print Cv
     </button>
