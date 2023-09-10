e8f6b70aa240598dccc553fc501962e7e0266bbe
Ilmaooo <ilmaogresevic1@gmail.com>
2023-09-10 13:45:54 +0200

    small changes in styles

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -13,7 +13,7 @@ function Skills() {
         </ul>
       </div>
       <div>
-        <h2 className="text-[#948E8E] uppercase text-2xl  tracking-wide mt-10 mb-5">
+        <h2 className="text-[#948E8E] uppercase text-xl sm:text-2xl  mt-10  tracking-wide mb-5 font-medium font-[montserrat]">
           soft skills
         </h2>
         <ul className="text-[#484545] font-medium">
